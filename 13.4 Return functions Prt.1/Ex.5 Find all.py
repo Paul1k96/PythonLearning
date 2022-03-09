d4bd546636444f578314495fa63d8a4cbfb4e93d
Paul1k96 <timofeev.pavel.art@gmail.com>
2022-03-09 18:15:37 +0300

    Add new exercises

---
+++ 13.4 Return functions Prt.1/Ex.5 Find all.py
@@ -0,0 +1,9 @@
+# объявление функции
+def find_all(target, symbol):
+    return [i for i in range(len(list(target))) if list(target)[i] == symbol]
+# считываем данные
+s = input()
+char = input()
+
+# вызываем функцию
+print(find_all(s, char))
