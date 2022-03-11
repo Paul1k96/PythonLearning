ad7ead9a02fb2c8a74025fa251f4afcb4bb9c5dc
Paul1k96 <timofeev.pavel.art@gmail.com>
2022-03-11 17:48:25 +0300

    Fix code

--- 13.5 Return functions Prt.2/Ex.8 Correct bracket sequence.py
+++ 13.5 Return functions Prt.2/Ex.8 Correct bracket sequence.py
@@ -1,1 +1,24 @@
-Делать через счётчик
+# объявление функции
+def is_correct_bracket(text):
+    text, count = list(text), 0
+    if text[0] == ')' or text[-1] == '(':
+        return False
+    for i in text:
+        if i == '(':
+            count+=1
+        if i == ')':
+            count-=1
+        if count == -1:
+            return False
+        #print(count)
+    if count == 0:
+        return True
+    else:
+        return False
+
+# считываем данные
+txt = input()
+
+# вызываем функцию
+print(is_correct_bracket(txt))
+#Делать через счётчик
