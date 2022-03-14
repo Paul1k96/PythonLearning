765ce66d85f850c469d142b5677f2a51be4c1640
Paul1k96 <timofeev.pavel.art@gmail.com>
2022-03-14 17:41:43 +0300

    Add new exercises

---
+++ 13.6 Return functions Prt.3/Ex.2 Area and Length.py
@@ -0,0 +1,12 @@
+# объявление функции
+def get_circle(radius):
+    from math import pi
+    return 2*pi*r, pi*r**2
+
+
+# считываем данные
+r = float(input())
+
+# вызываем функцию
+length, square = get_circle(r)
+print(length, square)
