0d0102294952cecb2750d5331a0d6089ee27e396
Paul1k96 <timofeev.pavel.art@gmail.com>
2022-03-17 18:59:40 +0300

    Code improvement

--- 15.2 Number Guesser/Ex.1 Number Guesser.py
+++ 15.2 Number Guesser/Ex.1 Number Guesser.py
@@ -1,51 +1,62 @@
-def number_guesser(guess):
+def number_guesser(a, b):
     count = 0
     from random import randint
-    n = randint(1, 100)
+    n = randint(1, b)
     count = 1
-    guess = int(guess)
-    while guess != n:
-        if guess > n:
+    a = int(a)
+    while a != n:
+        if a > n:
             print('Слишком много, попробуйте ещё раз!')
-            guess = input()
+            a = input()
             count += 1
-        elif guess < n:
+        elif a < n:
             print('Слишком мало, попробуйте ещё раз!')
-            guess = input()
+            a = input()
             count += 1
-        if is_valid(guess) == True:
-            guess = int(guess)
+        if is_valid(a, b) == True:
+            a = int(a)
         else:
-            while is_valid(guess) != True:
-                guess = input('А может быть все-таки введем целое число от 1 до 100?')
-            guess = int(guess)
+            while is_valid(a, b) != True:
+                print('А может быть все-таки введем целое число от 1 до', b, '? :')
+                a = input()
+            a = int(a)
     print('Число попыток:', count)
     return 'Вы угадали, поздравляем!'
 
-def is_valid(n):
-    if n.isdigit() == False:
+def is_valid(g, e):
+    if str(g).isdigit() == False:
         return False
-    return int(guess) in range(1,101)
+    return int(g) in range(1, int(e)+1)
 
+numb = input('Добро пожаловать! Это числовая угадайка. Задайте максимальный диапазон целым числом начиная от 1 до: ')
+while is_valid(numb, numb) != True:
+    if is_valid(numb, numb) == True:
+        break
+    else:
+        numb = input('Неверное значение. Введите целое число начиная от 1: ')
+
+print('Задан числовой диапазон от 1 до', numb)
+guess = input('Как думаете, какое число сейчас у меня в памяти? Ответ: ')
 again = 'да'
-guess = input('Это числовая угадайка в диапазоне от 1 до 100. Как думаете, какое число сейчас у меня в памяти? ')
 while guess != 'нет':
-    if is_valid(guess) == True:
+    if is_valid(guess, numb) == True:
+        guess = int(guess)
+        numb = int(numb)
 
         while again == 'да':
-            print(number_guesser(guess))
+            print(number_guesser(guess, numb))
 
-            while again != 'да' or again != 'нет':
+            while True:
                 again = input('Хотите ещё раз попробовать? Ответ "да" или "нет: "').lower()
                 if again == 'нет':
+                    break
+                elif again == 'да':
                     break
 
             if again == 'нет':
                 break
     else:
-        guess = input('Неверное значение. Введите число от 1 до 100, либо введите "нет", если хотите выйти.').lower()
+        guess = input('Неверное значение. Введите число от 1 до 100, либо введите "нет", если хотите выйти: ').lower()
     if again == 'нет':
         break
 print('Спасибо за игру!')
-
-
