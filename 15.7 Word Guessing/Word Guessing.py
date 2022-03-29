b4b49636814fd801db796e03720fa393b14bced4
Paul1k96 <timofeev.pavel.art@gmail.com>
2022-03-29 18:12:49 +0300

    Add new mini-project Word Guessing

---
+++ 15.7 Word Guessing/Word Guessing.py
@@ -0,0 +1,33 @@
+def get_word():
+    from random import randint
+    word_list = ['ЧЕЛОВЕК', 'ЖИВОТНОЕ', 'КОМПЬЮТЕР', 'КАБЕЛЬ', 'ЭТАНОЛ', 'АЛЬБАТРОС']
+    n = randint(len(word_list))
+    return n
+
+def check_cycle(a, b):
+    for i in range(len(a)):
+        if b == a[i]:
+            a[i] = b
+    return a[i]
+
+
+
+
+
+tries = 6
+print('Добро пожаловать в игру "Виселица". В данной игре нужно угадывать буквы, из которого состоит слово. Не угадываешь - теряешь попытку. Всего попыток 6.')
+print('Начальная позиция виселицы:', display_hangman(tries), sep='\n')
+print('Первая и последняя буквы даны. Угадаешь остальные?')
+word = get_word()
+hide_word = list(word[0]) + ['_' for i in range(1, len(word)-1)] + list(word[-1])
+print(''.join(hide_word))
+while True:
+    try_guess = input()
+    if try_guess in word:
+        hide_word = check_cycle(hide_word, try_guess)
+        print('Правильно! Оставшиеся буквы:', hide_word)
+    else:
+        tries-=1
+        print('Не верно!')
+        print(display_hangman(tries))
+        if tries < 0:
