b8946361db2fbc061b7b33f1bb4e0f1861fb1751
Paul1k96 <timofeev.pavel.art@gmail.com>
2022-03-30 16:48:11 +0300

    Work on Word Guessing

--- 15.7 Word Guessing/Word Guessing.py
+++ 15.7 Word Guessing/Word Guessing.py
@@ -1,14 +1,14 @@
 def get_word():
-    from random import randint
+    from random import randrange
     word_list = ['ЧЕЛОВЕК', 'ЖИВОТНОЕ', 'КОМПЬЮТЕР', 'КАБЕЛЬ', 'ЭТАНОЛ', 'АЛЬБАТРОС']
-    n = randint(len(word_list))
+    n = word_list[randrange(len(word_list))]
     return n
 
 def check_cycle(a, b):
     for i in range(len(a)):
         if b == a[i]:
             a[i] = b
-    return a[i]
+    return a
 
 def display_hangman(tries):
     stages = [ # финальное состояние: голова, торс, обе руки, обе ноги
@@ -48,7 +48,7 @@ def display_hangman(tries):
                    |      O
                    |     \\|
                    |      |
-                   |     / 
+                   |      
                    -
                ''',
                # голова, торс
@@ -58,7 +58,7 @@ def display_hangman(tries):
                    |      O
                    |      |
                    |      |
-                   |     / 
+                   |     
                    -
                ''',
                # голова
@@ -84,25 +84,38 @@ def display_hangman(tries):
           ]
     return stages[tries]
 
+def play(word, hide_word, tries):
+    while True:
+        try_guess = input()
+        if try_guess in word:
+            hide_word = check_cycle(hide_word, try_guess)
+            print('Правильно! Оставшиеся буквы:', hide_word)
+        else:
+            tries -= 1
+            print('Не верно!')
+            print(display_hangman(tries))
+            if tries < 0:
+                print('Загаданное слово:', word)
+                print('Хотите ещё раз сыграть? "Да" или "Нет":')
+                yes_no = input()
+                if yes_no == 'нет':
+                    return 'нет'
 
-tries = 6
+
+
 print('Добро пожаловать в игру "Виселица". В данной игре нужно угадывать буквы, из которого состоит слово. Не угадываешь - теряешь попытку. Всего попыток 6.')
-print('Начальная позиция виселицы:', display_hangman(tries), sep='\n')
-print('Первая и последняя буквы даны. Угадаешь остальные?')
-word = get_word()
-hide_word = list(word[0]) + ['_' for i in range(1, len(word)-1)] + list(word[-1])
-print(''.join(hide_word))
+
+
 while True:
-    try_guess = input()
-    if try_guess in word:
-        hide_word = check_cycle(hide_word, try_guess)
-        print('Правильно! Оставшиеся буквы:', hide_word)
-    else:
-        tries-=1
-        print('Не верно!')
-        print(display_hangman(tries))
-        if tries < 0:
-            print('Вы проиграли!')
-            print('Хотите ещё раз сыграть? "Да" или "Нет":')
-            yes_no = input()
+    tries = 6
+    word = get_word()
+    hide_word = ''.join(list(word[0]) + ['_' for i in range(1, len(word) - 1)] + list(word[-1]))
+    print('Начальная позиция виселицы:', display_hangman(tries), sep='\n')
+    print('Угадаешь остальные буквы слова?')
+    yes_no = play(word, hide_word, tries)
+    if yes_no == 'нет':
+        break
+
+print('Спасибо за игру!')
+
 
